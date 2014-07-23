b95951652f02714f073d9a1b0610b91915b8abf8
pr0stats developer <pr0stats@inbox.com>
2014-07-23 18:41:37 +0200

    Migrate templates into html file

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-// Google analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-53004741-1', 'auto');
-ga('send', 'pageview');
-
 $(document).ready(function() {
 	// Image toggle (show large image on click)
 	$('.image-toggle').hide();
@@ -147,3 +138,12 @@ $(function() {
 		}, "fast");
 	});
 });
+
+// Google analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-53004741-1', 'auto');
+ga('send', 'pageview');
--- js/template.js
+++ js/template.js
@@ -1,44 +1,13 @@
 $(document).ready(function() {
 	// Add header
-	var header = '<header>' +
-			'<div class="home"><a href="/">pr0stats - pr0gramm.com Statistiken</a></div> ' +
-			'<p class="meta"><a href="/motivation.html">Motivation</a> | ' +
-							'<a href="/methodik.html">Methodik</a> | ' +
-							'<a href="/analysen/index.html">Analysen</a> | ' +
-							'auf <a class="prg-link" target="_blank" href="http://pr0gramm.com/user/pr0stats">pr0gramm</a> | ' +
-							'auf <a href="https://github.com/pr0stats">github</a> | ' +
-							'auf <a target="_blank" href="https://twitter.com/pr0stats">twitter</a></p>' +
-		'</header>';
-	$('body').prepend(header);
+	$.get('/template/header.html', function(response) {
+		var header = response;
+		$('body').prepend(header);
+	});
 	
 	// Add footer
-	var footer = '<footer>' +
-			'<div class="left-box"><a href="#">top</a></div>' +
-			'<div class="bitcoin-donate center-box">' +
-				'<div style="font-size:16px;margin:0 auto;width:300px" class="blockchain-btn"' +
-					'data-address="1BmMRemMJ2mWS6Ppo92V2qEPxnWmH98vAe"' +
-					'data-shared="false">' +
-					'<div class="blockchain stage-begin">' +
-						'<img class="bitcoin-logo" src="/img/we-love-bitcoins.png"/>' +
-					'</div>' +
-					'<div class="blockchain stage-loading" style="text-align:center">' +
-						'<img src="https://blockchain.info//Resources/loading-large.gif"/>' +
-					'</div>' +
-					'<div class="blockchain stage-ready">' +
-						'<p align="center">' +
-							'Spendenadresse: <b>[[address]]</b>' +
-						'</p>' +
-						'<p align="center" class="qr-code"></p>' +
-					'</div>' +
-					'<div class="blockchain stage-paid">' +
-						'Spende in Höhe von <b>[[value]] BTC</b> erhalten. Danke!' +
-					'</div>' +
-					'<div class="blockchain stage-error">' +
-						'<font color="red">[[error]]</font>' +
-					'</div>' +
-				'</div>' +
-			'</div>' +
-			'<div class="right-box"><a href="LICENSE">unlizensiert</a></div>' +
-		'</footer>';
-	$('body').append(footer);
+	$.get('/template/footer.html', function(response) {
+		var footer = response;
+		$('body').append(footer);
+	});
 });
