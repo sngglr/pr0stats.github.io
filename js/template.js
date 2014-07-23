b87de83fdcf6e4766ec797155d12296096cd189a
pr0stats developer <pr0stats@inbox.com>
2014-07-23 19:54:36 +0200

    Fix reloading BTC donate area from template

--- js/template.js
+++ js/template.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 	// Add header
-	$.get('/template/header.html', function(response) {
+	$.get('template/header.html', function(response) {
 		var header = response;
 		$('body').prepend(header);
 	});
 	
 	// Add footer
-	$.get('/template/footer.html', function(response) {
+	$.get('template/footer.html', function(response) {
 		var footer = response;
 		$('body').append(footer);
 	});
