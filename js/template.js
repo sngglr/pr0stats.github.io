d51eb0ad71e9996a218c2d5fc87c907108086cdf
pr0stats developer <pr0stats@inbox.com>
2014-07-24 15:14:13 +0200

    Add edit link to footer

--- js/template.js
+++ js/template.js
@@ -9,5 +9,18 @@ $(document).ready(function() {
 	$.get('/template/footer.html', function(response) {
 		var footer = response;
 		$('body').append(footer);
+		
+		// Add edit link to every page with link to github source
+		var githubRepoTarget = 'https://github.com/pr0stats/pr0stats.github.io/blob/master/';
+		var currentUrl = document.URL.split('/');
+		var query = currentUrl.splice(4, (currentUrl.length-1));
+		
+		if(query.length == 1 && query[0] == "") {
+			query[0] = 'index.html';
+		}
+
+		var target = githubRepoTarget + query.join('/');
+		var editLink = $('.edit-link a');
+		editLink.attr('href', target);
 	});
 });
