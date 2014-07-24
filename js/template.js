034a99d38ae4222e39f6d4707b5ba43bec5d5d87
pr0stats <pr0stats@users.noreply.github.com>
2014-07-24 15:17:31 +0200

    Fix template.js attempt

--- js/template.js
+++ js/template.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 		// Add edit link to every page with link to github source
 		var githubRepoTarget = 'https://github.com/pr0stats/pr0stats.github.io/blob/master/';
 		var currentUrl = document.URL.split('/');
-		var query = currentUrl.splice(4, (currentUrl.length-1));
+		var query = currentUrl.splice(3, (currentUrl.length-1));
 		
 		if(query.length == 1 && query[0] == "") {
 			query[0] = 'index.html';
